b982428b36f4263740da479f62b14064e0c4e19f
Jamil Seddiqi <jamilsediq@gmail.com>
2019-11-19 20:52:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -52,3 +52,8 @@ end
 
   instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
   instruments.flatten
+end 
+
+instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+instructors.delete ("Steven")
+
